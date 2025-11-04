f100b8be3f4c8902d6edbfc8e4e1c46541b4a4b2
Andre Amor <ramorSTEM2022@gmail.com>
2025-11-03 23:56:41 -0800

    Update tab and shift width to 2

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("config.lazy")
 
 vim.o.number = true
 vim.o.relativenumber = true 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
